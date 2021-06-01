48b211c93393dc4b1943657b2b4fd66533359749
anaksnascimento <69437887+anaksnascimento@users.noreply.github.com>
2021-05-31 22:06:33 -0300

    31-05-21

---
+++ src/components/Content.tsx
@@ -0,0 +1,33 @@
+import { MovieCard } from "./MovieCard";
+
+import { MovieProps } from "../App";
+import { Header } from "./Header";
+
+interface Conteudo {
+  content: {
+    title: string;
+    movies: MovieProps[];
+  };
+}
+export function Content(props: Conteudo) {
+  return (
+    <>
+      <div className="container">
+        <Header genre={{ title: props.content.title }} />
+        <main>
+          <div className="movies-list">
+            {props.content.movies.map((movie) => (
+              <MovieCard
+                key={movie.imdbID}
+                title={movie.Title}
+                poster={movie.Poster}
+                runtime={movie.Runtime}
+                rating={movie.Ratings[0].Value}
+              />
+            ))}
+          </div>
+        </main>
+      </div>
+    </>
+  );
+}
